9af4f021a39d8140372872a5ff684aebd2707734
lucap84 <lucap84@gmail.com>
2016-05-23 15:44:05 +0200

    piccoli change su salvataggio dto in json

--- it.fff.client.mobile/src/test/java/it/fff/client/test/stub/WebServiceRestTest.java
+++ it.fff.client.mobile/src/test/java/it/fff/client/test/stub/WebServiceRestTest.java
@@ -2,6 +2,8 @@ package it.fff.client.test.stub;
 
 import java.io.File;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -12,6 +14,8 @@ import it.fff.clientserver.common.util.FlokkerUtils;
 
 public class WebServiceRestTest{
 	
+	private static final Logger logger = LogManager.getLogger(WebServiceRestTest.class);
+
 	private String pathForJsonDTO;
 	private ObjectMapper jsonMapper;
 	
@@ -19,14 +23,18 @@ public class WebServiceRestTest{
 		pathForJsonDTO = ClientConfiguration.getInstance().getClientConfigurationProperties().getProperty(ClientConstants.PROP_DTO_JSON_DIR);
 		this.createDirIfNotExists(pathForJsonDTO);
 		this.jsonMapper = new ObjectMapper();
+		logger.info("WebServiceRestTest inizializzato. Path per JSON files: "+pathForJsonDTO);
 	}
 
 	public void saveJsonResult(Object result, String filename) {
 		try {
 			String jsonString = this.jsonMapper.writeValueAsString(result);
 			
-			File f = new File(this.pathForJsonDTO+filename+".json");
+			String completeFilename = filename+".json";
+			
+			File f = new File(this.pathForJsonDTO+completeFilename);
 			FlokkerUtils.writeStringToFile(f,jsonString);
+			logger.info("JSON risultato salvato su file: "+completeFilename);
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
 		}
